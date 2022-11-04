83b758a0aab7c68f34c84bbc74feafa552b78ffa
basicMindset <laczkoo1993@gmail.com>
2022-11-04 16:39:15 +0100

    finished

--- code/hangman.py
+++ code/hangman.py
@@ -1,3 +1,4 @@
+from re import M
 from getWord import getWordForGame as gWFG
 import string
 
@@ -6,27 +7,37 @@ class guessCompare():
         self.word = word
         self.word_letters = set(word)
 
-    def check_user_input(self):
+    def play(self):
         alphabet = set(string.ascii_uppercase)
         used_letters = set()
 
-        while len(self.word_letters) > 0:
+        lives = 7
+
+        while len(self.word_letters) > 0 and lives > 0:
             #print used letters
-            print('Already used letters: ', ', '.join(used_letters))
+            print('You have', lives, ' lives left and you have already used these letters: ', ', '.join(used_letters))
 
-            word_letter = [letter if letter in used_letters else '-' for letter in self.word]
-            print(word_letter)
+            word_letter = [letter if letter in used_letters else '_' for letter in self.word]
+            print('Current word:', ' '.join(word_letter))
 
-            user_input = input("\nGive me an alphabet: ").upper()
+            user_input = input("Give me an alphabet: ").upper()
             if user_input in alphabet - used_letters:
                 used_letters.add(user_input)
                 if user_input in self.word_letters:
                     self.word_letters.remove(user_input)
+                else:
+                    lives = lives - 1
+                    if lives > 0:
+                        print('Alphabet is not in the word.')
+
             elif user_input in used_letters:
                 print('Alphabet already used. Choose another one.')
             else: 
                 print('Invalid character. Try again')
+        if lives == 0:
+            print('No more lives left. You lose.')
+        else:
+            print('You guessed the word correctly:', self.word)
             
-word = gWFG().returnRandomWord()
-module = guessCompare(word)
-module.check_user_input()
+module = guessCompare(gWFG().returnRandomWord())
+module.play()
